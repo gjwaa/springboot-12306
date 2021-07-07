2a163cb87625a9adc401b4af7af3e32bf55f82e9
gjwaa <1258046117@qq.com>
2021-07-08 00:21:28 +0800

    first

--- src/main/java/com/gjw/controller/LoginController.java
+++ src/main/java/com/gjw/controller/LoginController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -31,10 +32,25 @@ public class LoginController {
         list.add(new UserInfo(2, "张四", 11, "广州"));
         list.add(new UserInfo(3, "张五", 12, "北京"));
         list.add(new UserInfo(4, "张六", 13, "上海"));
+        list.add(new UserInfo(5, "张三", 10, "福建"));
+        list.add(new UserInfo(6, "张四", 11, "广州"));
+        list.add(new UserInfo(7, "张五", 12, "北京"));
+        list.add(new UserInfo(8, "张六", 13, "上海"));
+        list.add(new UserInfo(9, "张三", 10, "福建"));
+        list.add(new UserInfo(10, "张四", 11, "广州"));
+        list.add(new UserInfo(11, "张五", 12, "北京"));
+        list.add(new UserInfo(12, "张六", 13, "上海"));
 //        JSONObject jsonObject = new JSONObject();
 //        jsonObject.put("allInfos", list);
 //        return jsonObject.toString();
+
         return list;
+    }
+
+    @RequestMapping("userTable")
+    public List userTable() {
+
+        return Arrays.asList("厦门", "三明", "福州");
     }
 
 }
